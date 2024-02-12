973600c92625ea137a204dbd00bfcf5c7d4f0886
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2024-02-12 11:46:08 +0900

    ランキング関連の修正

--- DriveAndAvoid1/DriveAndAvoid1/Scene/RankingInputScene.cpp
+++ DriveAndAvoid1/DriveAndAvoid1/Scene/RankingInputScene.cpp
@@ -189,7 +189,7 @@ bool RankingInputScene::InputName()
 		}
 		else if (cursor_y < 4)
 		{
-			name[name_num++] = 'a' + cursor_x + ((cursor_y - 2) * 13);
+			name[name_num++] = 'A' + cursor_x + ((cursor_y - 2) * 13);
 			if (name_num == 14)
 			{
 				cursor_x = 0;
