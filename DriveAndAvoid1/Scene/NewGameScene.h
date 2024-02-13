17c9cee98b7a040cc59a35d8ede9dc282ab685cf
入仲 <122337550+Irinaka1021@users.noreply.github.com>
2024-02-13 09:49:20 +0900

    newgame

--- DriveAndAvoid1/Scene/NewGameScene.h
+++ DriveAndAvoid1/Scene/NewGameScene.h
@@ -1,5 +1,19 @@
 #pragma once
-class NewGameScene
+#include"SceneBase.h"
+class NewGameScene : public SceneBase
 {
+private:
+	
+
+public:
+	NewGameScene();
+	virtual ~NewGameScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
 };
 
